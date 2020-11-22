7ca50f4513ab1c68702e5454adc650d6f0868b86
Your Name <you@example.com>
2020-11-23 00:28:46 +0700

    Bao cao Gk

---
+++ app/src/main/java/com/nghia/pvn/baicuoiky/HoaDonActivity.java
@@ -0,0 +1,28 @@
+package com.nghia.pvn.baicuoiky;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HoaDonActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hoa_don);
+
+        ImageButton bt_back = (ImageButton) findViewById(R.id.btn_back);
+        bt_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                open(view);
+            }
+        });
+    }
+    void open(View v){
+        Intent Intent = new Intent(this, activity5.class);
+        startActivity(Intent);
+    }
+}
